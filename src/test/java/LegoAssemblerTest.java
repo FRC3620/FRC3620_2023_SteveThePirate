40c01e6e79d87418bf00e5f8501ef05577e7a17f
AJ <10586@stjoebears.com>
2023-02-20 20:52:18 -0500

    updated legos

--- src/test/java/LegoAssemblerTest.java
+++ src/test/java/LegoAssemblerTest.java
@@ -4,6 +4,7 @@ import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import frc.robot.CannonLocation;
 import frc.robot.LegoAssembler;
 import frc.robot.RobotContainer;
 import frc.robot.TargetPoseOnField;
@@ -13,6 +14,7 @@ import frc.robot.commands.CannonExtendCommand;
 import frc.robot.commands.CannonPitchCommand;
 import frc.robot.commands.DriveToCoordinateCommand;
 import frc.robot.commands.SetCannonClawSpeedCommand;
+import frc.robot.commands.SetCannonLocationCommand;
 import frc.robot.subsystems.CannonSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -53,27 +55,26 @@ public class LegoAssemblerTest {
             add("check odometry");
 
             //add(new DriveToCoordinateCommand(startPosition.getTargetPoseOnField(), 0.1, 0.1, 0, driveSubsystem));
-            //moves in front of peg
-            //mostly placeholder numbers
-            //use CannonSetLocationCommand when created to replace 60, 61, 62 and all duplicates
+
+            //mostly placeholder numbers, CANNONLOCATION NUMBERS ARE NOT TESTED
+
             add(new DriveToCoordinateCommand(TargetPoseOnField.target(startPosition, -1, -1), 0.1, 0.1, 0, driveSubsystem), "Move in front of peg");
-            add(new CannonElevateCommand(cannonSubsystem, 60),"Set desired cannon height for deposit");
-            add(new CannonPitchCommand(cannonSubsystem, -20), "Set desired cannon pitch for deposit");
-            add(new CannonExtendCommand(cannonSubsystem, 7), "Set desired cannon length for deposit");
+            add(new SetCannonLocationCommand(CannonLocation.coneHighLocation),"Set Cannon to deposit position");
             add(new SetCannonClawSpeedCommand(cannonSubsystem, -0.1), "Deposit Game Piece");
-            add(new DriveToCoordinateCommand(TargetPoseOnField.target(startPosition, -5, -1), 0.1, 0.1, 180, driveSubsystem), "Exit community and get in front of game piece");
-            add(new CannonElevateCommand(cannonSubsystem, 90),"Set desired cannon height for pickup");
-            add(new CannonPitchCommand(cannonSubsystem, 0), "Set desired cannon pitch for pickup");
-            add(new CannonExtendCommand(cannonSubsystem, 3), "Set desired cannon length for pickup");
+            add(new SetCannonLocationCommand(CannonLocation.parkLocation),"Set Cannon to vertical position");
+            add(new DriveToCoordinateCommand(TargetPoseOnField.target(startPosition, -5, -1), 0.1, 0.1, 180, driveSubsystem), "Exit community");
+            
+            //extra in case we get this far
+            add(new SetCannonLocationCommand(CannonLocation.floorPickLocation),"Set Cannon to pickup position");
             add(new SetCannonClawSpeedCommand(cannonSubsystem, 0.2), "Pickup Game Piece");
+            add(new SetCannonLocationCommand(CannonLocation.parkLocation),"Set Cannon to vertical position");
             add(new DriveToCoordinateCommand(TargetPoseOnField.target(startPosition, -1, -1), 0.1, 0.1, 0, driveSubsystem), "Move in front of peg");
-            add(new CannonElevateCommand(cannonSubsystem, 60),"Set desired cannon height for deposit");
-            add(new CannonPitchCommand(cannonSubsystem, -20), "Set desired cannon pitch for deposit");
-            add(new CannonExtendCommand(cannonSubsystem, 7), "Set desired cannon length for deposit");
+            add(new SetCannonLocationCommand(CannonLocation.coneHighLocation),"Set Cannon to deposit position");
             add(new SetCannonClawSpeedCommand(cannonSubsystem, -0.1), "Deposit Game Piece");
+            add(new SetCannonLocationCommand(CannonLocation.parkLocation),"Set Cannon to vertical position");
 
             //Whatever comes next
-            
+
         }
     }
 }
