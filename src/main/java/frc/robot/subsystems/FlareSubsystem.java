09a54e33f59b973e04f189d8d85166e0919cf56f
Mariah <11564@stjoebears.com>
2023-02-23 20:56:39 -0500

    flare lights with neo pixel strips

--- src/main/java/frc/robot/subsystems/FlareSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlareSubsystem.java
@@ -6,52 +6,79 @@ import org.usfirst.frc3620.logger.EventLogging.Level;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FlareSubsystem extends SubsystemBase {
-  private Spark blinkinMotorController;
-  private FlareColor currentFlareColor;
+  
   AddressableLED leds;
   AddressableLEDBuffer ledBuffer;
 
   Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
 
-  Timer timer = new Timer();
-
   public FlareSubsystem() {
-    blinkinMotorController = new Spark(2);
-    addChild("Blinkin", blinkinMotorController);
-    currentFlareColor = FlareColor.YELLOW;
 
     leds = new AddressableLED(9);
     leds.setLength(8);;
     ledBuffer = new AddressableLEDBuffer(8);
-
-    timer.start();
-
+    setColor(new FlareColor(Color.kBlue));
+    leds.start();
+  }
+  public void setColor(FlareColor flareColor){
+    logger.info ("Set color to {}", flareColor);
 
     for (var i = 0; i < ledBuffer.getLength(); i++) {
       // Sets the specified LED to the RGB values for red
-      ledBuffer.setRGB(i, 255, 0, 0);
+      ledBuffer.setRGB(i, flareColor.getRed(), flareColor.getGreen() , flareColor.getBlue());
    }
     leds.setData(ledBuffer);
-    leds.start();
   }
 
-  public void setColor(FlareColor flareColor){
+  public void setColor(FlareColor flareColor, int firstLed, int lastLed){
     logger.info ("Set color to {}", flareColor);
-    currentFlareColor = flareColor;
+
+    if (firstLed < 0) {
+      firstLed = 0;
+    }
+    if (lastLed >= ledBuffer.getLength()) {
+      lastLed = ledBuffer.getLength() - 1;
+    }
+
+    for (var i = firstLed; i <= lastLed; i++) {
+      // Sets the specified LED to the RGB values for red
+     
+      ledBuffer.setRGB(i, flareColor.getRed(), flareColor.getGreen() , flareColor.getBlue());
+   }
+    leds.setData(ledBuffer);
   }
 
-  public enum FlareColor{
-    PURPLE(0.91), YELLOW(0.69), YELLOWSTROBE(0.15), PURPLESTROBE(0.35);
+  public static class FlareColor{
+    //PURPLE(0.91), YELLOW(0.69), YELLOWSTROBE(0.15), PURPLESTROBE(0.35);
+    public static FlareColor PURPLESTROBE = new FlareColor (Color.kPurple);
+
+    public static FlareColor YELLOWSTROBE = new FlareColor (Color.kYellow);
+
+
   
-    public final double value;
-    private FlareColor(double value){
-      this.value = value;
+    public final Color value;
+    public FlareColor(int R, int G, int B){
+      this.value = new Color(R, G, B);
     }
+    public FlareColor(Color color){
+      this.value = color;
+    }
+
+    public int getRed() {
+      return (int) Math.round(this.value.red * 255);
+    }
+
+    public int getGreen(){
+      return (int) Math.round(this.value.green * 255);
+    }
+    public int getBlue(){
+      return (int) Math.round(this.value.blue * 255);
+    }
+  
   }
 
  // int m_rainbowFirstPixelHue = 0;
@@ -61,39 +88,10 @@ public class FlareSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    blinkinMotorController.set(currentFlareColor.value);
+    
 
     // For every pixel
 
-    for (var i = 0; i < ledBuffer.getLength();i++) {
-
-      // Calculate the hue - hue is easier for rainbows because the color
-
-      // shape is a circle so only one value needs to precess
-    
-        var hue = (m_purpleFirstPixleHue + (i * 95 / ledBuffer.getLength())) % 91;
-
-    //  var hue = (m_rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
-
-      // Set the value
-
-    //  ledBuffer.setHSV(i, hue, 255, 128);
-
-    ledBuffer.setHSV(i, 298, 100, 40);
-
-    }
-   // leds.setData(ledBuffer);
-    leds.setData(ledBuffer);
-
-    // Increase by to make the rainbow "move"
-
-   // m_rainbowFirstPixelHue += 3;
    
-
-    // Check bounds
-
-   // m_rainbowFirstPixelHue %= 180;
-   m_purpleFirstPixleHue %= 91
-   ;
   }
 }
