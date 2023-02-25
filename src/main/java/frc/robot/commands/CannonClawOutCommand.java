958f4830a888b36b3dd95d1b82df5c1ad209e0a8
Mic <micnc2006@gmail.com>
2023-02-25 14:51:12 -0500

    Fine tuned the presets and made intake "grippier"

---
+++ src/main/java/frc/robot/commands/CannonClawOutCommand.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.CannonSubsystem;
+
+public class CannonClawOutCommand extends CommandBase {
+    /** Creates a new CannonExtendCommand. */
+    CannonSubsystem cannonSubsystem;
+    double desiredSpeed;
+    Timer getStartedTimer;
+    /**
+     * 
+     * Creates a new MoveTurretCommand.
+     */
+    public CannonClawOutCommand(CannonSubsystem _subsystem, double _desiredSpeed) {
+      // Use addRequirements() here to declare subsystem dependencies.
+      //addRequirements(_subsystem);
+      cannonSubsystem = _subsystem;
+      desiredSpeed = _desiredSpeed;
+    } 
+  
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+      cannonSubsystem.setClawSpeed(desiredSpeed);
+    }
+  
+    // Called every time the scheduler runs while the command is scheduled.
+  
+    @Override
+    public void execute() 
+    {
+      cannonSubsystem.setClawSpeed(desiredSpeed);
+    }
+  
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+      cannonSubsystem.setClawSpeed(0);
+    }
+  
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+         return false;
+    }
+  }
