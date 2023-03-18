edc43dd1f0075b3a52f7905ff7d926615c816940
Manasvi <12220@stjoebears.com>
2023-03-18 17:33:20 -0400

    fixed set position

--- src/main/java/frc/robot/CannonLocation.java
+++ src/main/java/frc/robot/CannonLocation.java
@@ -12,11 +12,11 @@ public class CannonLocation {
     public static CannonLocation coneMidLocation = new CannonLocation(44.33, 13.61, -66.78);
     public static CannonLocation cubeMidLocation = new CannonLocation(42.2, 7.8, -67.1);
     public static CannonLocation lowLocation = new CannonLocation(3.5, 0, -67);
-    public static CannonLocation coneFloorPickLocation = new CannonLocation(-10, 0, -2.29);
-    public static CannonLocation chuteLocation = new CannonLocation(38.7, 0.82, -20);
-    public static CannonLocation stationLocation = new CannonLocation(69.3, 13.9, -103);
+    public static CannonLocation coneFloorPickLocation = new CannonLocation(-10.1, 0.0,5.0);
+    public static CannonLocation chuteLocation = new CannonLocation(38.7, 0.82, -1.57);
+    public static CannonLocation stationLocation = new CannonLocation(69.3, 8.89, -67);
     public static CannonLocation parkLocation = new CannonLocation(90, 1, -90);
-    public static CannonLocation sidewaysConeLocation = new CannonLocation(-22, 2.27, 7.33);
+    public static CannonLocation sidewaysConeLocation = new CannonLocation(-31.59, 0.0, 27.5);
     double elevation;
     double extension;
     double wristPitch;
